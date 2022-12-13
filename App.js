08718ed8acef6c66d40e863cc6eb8b3d18e9ee86
rensilva5 <renzoasilva@gmail.com>
2022-12-13 01:55:05 -0500

    Creating first styles

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text>Hello React Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+      <Text style={styles.text}>This is Renzo creating</Text>
+      <Image source={{uri: 'https://randomuser.me/api/portraits/men/9.jpg'}} style={styles.img}/>
     </View>
   );
 };
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "darkblue",
+    fontSize: 35,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  }
+})
 
 export default App;
