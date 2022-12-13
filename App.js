3decd75b885bada1af0c74f881ed70832ce28b8d
rensilva5 <renzoasilva@gmail.com>
2022-12-13 14:06:30 -0500

    creating components and props

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello React Native</Text>
-      <Text style={styles.text}>This is Renzo creating</Text>
-      <Image source={{uri: 'https://randomuser.me/api/portraits/men/9.jpg'}} style={styles.img}/>
+      <Header title='My Shopping List'/>
     </View>
   );
 };
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 60,
   },
-  text: {
-    color: "darkblue",
-    fontSize: 35,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
-  }
-})
+});
 
 export default App;
